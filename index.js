b88c4327ef1098abf333396ef721a5405d9b7f8e
Mike Mols <mm@firtal.com>
2019-12-16 07:54:01 +0100

    changes endpoint

--- index.js
+++ index.js
@@ -9,5 +9,5 @@ const app = express();
 server.applyMiddleware({ app });
 
 app.listen({ port: 4000 }, () =>
-  console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`)
+  console.log(`🚀 Server ready at http://localhost:4000`)
 );
